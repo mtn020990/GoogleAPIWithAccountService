ce5daa7937667f333974379bc72a930157030d12
nganmai <ngan@zenfolio.com>
2025-03-07 18:38:55 +0700

    add google Index

--- GoogleServiceAccount/Controllers/GoogleSearchConsoleController.cs
+++ GoogleServiceAccount/Controllers/GoogleSearchConsoleController.cs
@@ -5,6 +5,12 @@ using Google.Apis.SearchConsole.v1.Data;
 using Google.Apis.Services;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Google.Apis.Indexing.v3;
+using System;
+using Google.Apis.Requests;
+using Google.Apis.Indexing.v3.Data;
+using Google.Apis.Discovery;
+using static Google.Apis.Indexing.v3.UrlNotificationsResource;
 
 namespace GoogleServiceAccount.Controllers
 {
@@ -13,6 +19,7 @@ namespace GoogleServiceAccount.Controllers
     public class GoogleSearchConsoleController : ControllerBase
     {
         private static SearchConsoleService searchConsoleService;
+        private static IndexingService indexingService;
         public GoogleSearchConsoleController()
         {
 
@@ -26,7 +33,10 @@ namespace GoogleServiceAccount.Controllers
         [HttpGet(Name = "GoogleSerchConsole")]
         public async Task<ActionResult> Get(string url)
         {
-            string siteUrl = "sc-domain:yoursomain.com";
+            await GoogleIndexing();
+            return Ok();
+
+            string siteUrl = "sc-domain:zenfoliosite.com";
             using (var stream =
               new FileStream("thinking-pagoda-447206-p8-0b562cdd5548.json", FileMode.Open, FileAccess.Read))
             {
@@ -45,20 +55,87 @@ namespace GoogleServiceAccount.Controllers
                     HttpClientInitializer = credentials
                 });
 
-                //var response = searchConsoleService.UrlInspection.Index.Inspect(
-                //    new InspectUrlIndexRequest
-                //    {
-                //        SiteUrl = siteUrl,
-                //        InspectionUrl = url
-                //    })
-                //    .Execute();
+                var response = await searchConsoleService.UrlInspection.Index.Inspect(
+                    new InspectUrlIndexRequest
+                    {
+                        SiteUrl = siteUrl,
+                        InspectionUrl = url
+                    })
+                    .ExecuteAsync();
 
-                 var result = searchConsoleService.Sitemaps.List(siteUrl).Execute();
+                var result = searchConsoleService.Sitemaps.List(siteUrl).Execute();
 
                 // var sitemap = searchConsoleService.Sitemaps.Get(siteUrl, url).Execute();
             }
 
             return Ok();
+        }
+
+        private async Task GoogleIndexing()
+        {
+            using (var stream =
+              new FileStream("thinking-pagoda-447206-p8-0b562cdd5548.json", FileMode.Open, FileAccess.Read))
+            {
+                var credentials = GoogleCredential.FromStream(stream);
+                if (credentials.IsCreateScopedRequired)
+                {
+                    credentials = credentials.CreateScoped(new string[]
+                    {
+                       IndexingService.Scope.Indexing
+                    });
+                }
+
+                indexingService = new IndexingService(new BaseClientService.Initializer()
+                {
+                    HttpClientInitializer = credentials
+                });
+
+                // var request = new BatchRequest(indexingService);
+
+                //var response = await indexingService.UrlNotifications.Publish(new Google.Apis.Indexing.v3.Data.UrlNotification
+                //{
+                //    Url = "",
+                //    Type = "URL_UPDATED"
+                //}).ExecuteAsync();
+
+                var urlToIndex = "";
+
+                var request = indexingService.UrlNotifications;
+
+                //var pub_response = await request.Publish(new UrlNotification
+                //{
+                //    Url = urlToIndex,
+                //    Type = "URL_UPDATED"
+                //}).ExecuteAsync();
+
+
+
+                GetMetadataRequest meta = request.GetMetadata();
+                meta.Url = urlToIndex;
+                UrlNotificationMetadata m = await meta.ExecuteAsync();
+            }
+        }
+
+        private void Scope()
+        {
+            //foreach (FeatureProjectEnum project in Enum.GetValues<FeatureProjectEnum>())
+            //{
+            //    serviceBuilder.ContainerBuilder.Register(ctx =>
+            //    {
+            //        FlagsmithSettings flagsmithSettings = ctx.Resolve<IOptions<FlagsmithSettings>>().Value.Validate();
+            //        string key = flagsmithSettings.GetKey(project);
+
+            //        if (string.IsNullOrEmpty(key))
+            //            throw new InvalidOperationException($"Flagsmith key is not found for \"{project}\"");
+
+            //        return new FlagsmithClient(
+            //            key,
+            //            retries: flagsmithSettings.FlagsmithRetry,
+            //            requestTimeout: flagsmithSettings.FlagsmithTimeout);
+            //    }).Keyed<FlagsmithClient>(project).SingleInstance();
+            //}
+
+            // FlagsmithClient flagsmithClient = _lifetimeScope.ResolveKeyed<FlagsmithClient>(project);
         }
     }
 }
